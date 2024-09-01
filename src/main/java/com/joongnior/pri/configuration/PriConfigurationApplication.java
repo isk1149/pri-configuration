0a6aa8270ea2722c7fd039cc42031c37085d1d2f
Inseong Kim <isk1149@naver.com>
2024-09-01 16:07:29 +0900

    @EnableConfigServer 설정

--- src/main/java/com/joongnior/pri/configuration/PriConfigurationApplication.java
+++ src/main/java/com/joongnior/pri/configuration/PriConfigurationApplication.java
@@ -2,8 +2,10 @@ package com.joongnior.pri.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer //Spring Cloud Config Server 설정
 public class PriConfigurationApplication {
 
 	public static void main(String[] args) {
